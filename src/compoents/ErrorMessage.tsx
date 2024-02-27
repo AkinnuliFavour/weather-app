8720c15fd420f993d34ded2a1c89d74a74da7749
AkinnuliFavour <123216973+AkinnuliFavour@users.noreply.github.com>
2024-02-27 14:01:33 +0000

    Update error message text

--- src/compoents/ErrorMessage.tsx
+++ src/compoents/ErrorMessage.tsx
@@ -1,7 +1,7 @@
 const ErrorMessage = () => {
   return (
     <main className='p-8 min-h-screen w-full bg-gradient-to-tr from-[#00B4FF] to-[#7A5CC1] text-gray-700 flex justify-center items-center'>
-       <p className="text-center font-bold">Sorry, but you are currently not connected to the internet. Please make sure you are connected to the internet then, refresh the page. Thanks.</p>
+       <p className="text-center font-bold">Please ensure that you are connected to the internet and you turn on location access. Thanks.</p>
     </main>
   )
 }
