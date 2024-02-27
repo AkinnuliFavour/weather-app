5d66d2aeefab6c7d9fe70c612787deb87ecd937e
AkinnuliFavour <123216973+AkinnuliFavour@users.noreply.github.com>
2024-02-27 13:04:24 +0000

    add search icon to search bar

--- src/compoents/SearchBar.tsx
+++ src/compoents/SearchBar.tsx
@@ -1,11 +1,27 @@
+import { FaSearch } from "react-icons/fa"
+
 const SearchBar = ({location, setLocation}: {location: string, setLocation: React.Dispatch<React.SetStateAction<string>>}) => {
     const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         console.log(`User Pressed ${e.key}`)
     }
     return (
-        <form action="" className='mt-10 flex justify-center items-center gap-4'> 
-            <input type="search" name="" id="" placeholder="Location Search" className="p-2 w-full lg:w-1/2 outline-none rounded-3xl shadow-inner bg-[#33C3FF]" onKeyDown={handleKeyDown} value={location} onChange={(e) => setLocation(e.target.value)}/>
-        </form>
+    <form action="" className="mt-10 flex justify-center items-center gap-4">
+      <input
+        type="search"
+        name=""
+        id=""
+        placeholder="Location Search"
+        className="p-2 w-full lg:w-1/2 outline-none rounded-3xl shadow-inner bg-[#33C3FF]"
+        onKeyDown={handleKeyDown}
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+
+      <FaSearch
+        onClick={() => console.log("Clicked")}
+        className="absolute top-1/2 transform -translate-y-1/2 right-2 h-5 w-5 text-blue-500 cursor-pointer"
+      />
+    </form>
     )
 }
 
